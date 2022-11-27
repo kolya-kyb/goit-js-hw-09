73af4193ea0d602d5a89339d0dfde83722213dde
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2022-11-27 12:37:31 +0200

    dz 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+refs.startButton.addEventListener('click', e => {
+  refs.stopButton.removeAttribute('disabled');
+  e.currentTarget.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
 
+refs.stopButton.addEventListener('click', e => {
+  refs.startButton.removeAttribute('disabled');
+  e.currentTarget.setAttribute('disabled', true);
+  clearInterval(timerId);
+});
